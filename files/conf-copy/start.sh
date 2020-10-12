0299228dfce5c1e95c278e28eb372a932401bac9
dugembot <72567572+dugembot@users.noreply.github.com>
2020-10-13 03:13:16 +0700

    Update start.sh

--- files/conf-copy/start.sh
+++ files/conf-copy/start.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-if [ ! -f /conf/aria2.conf ]; then
-    cp /conf-copy/aria2.conf /conf/aria2.conf
     if [ "${SECRET}" ]; then
-        echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
+        echo "rpc-secret=${SECRET}" >> /app/aria2.conf
     fi
-fi
 
-touch /conf/dht.dat /conf/dht6.dat /conf/aria2.session
+touch /conf/aria2.session
 
-aria2c --conf-path=/conf/aria2.conf
+aria2c --conf-path=/app/aria2.conf&
